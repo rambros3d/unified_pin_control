ad44b5f0ebdab54c17caa82e29bc1ebea6653769
RamBros <57451418+rambros3d@users.noreply.github.com>
2026-04-14 01:44:16 +0530

    chore: set vite base path and outDir for GitHub Pages

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,6 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/unified_pin_control/',
+  build: {
+    outDir: '../dist',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
